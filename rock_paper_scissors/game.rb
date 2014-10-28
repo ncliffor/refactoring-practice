1cdf2eac74e7752f60b0c8fc67f880027e45aa7a
Nick Clifford <ncliffor@gmail.com>
2014-10-28 15:10:17 -0400

    Display choices, print what computer and user chose

---
+++ rock_paper_scissors/game.rb
@@ -0,0 +1,47 @@
+class Game
+  CHOICES = ["R", "P", "S"]
+  QUIT = ["press q to quit"]
+
+  def play
+    give_user_choices
+    get_user_choice
+    computer_chooses
+    display_outcome
+  end
+
+  def give_user_choices
+    puts "Your move, what do you choose?"
+    options
+  end
+
+  def options
+    print_options
+    print " |"
+    puts
+    puts QUIT
+  end
+
+  def print_options
+    CHOICES.each do |choice|
+      print " | " + choice
+    end
+  end
+
+  def get_user_choice
+    @users_choice = gets.chomp.upcase
+  end
+
+  def computer_chooses
+    @computers_play = CHOICES.sample(1)
+  end
+
+  def display_outcome
+    puts "You played #{@users_choice}"
+    puts "The computer played #{@computers_play}"
+  end
+
+end
+
+game = Game.new
+game.play
+
