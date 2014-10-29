13d8e6acf49904b4886adf1b5bddb5eda6e3746e
Nick Clifford <ncliffor@gmail.com>
2014-10-29 10:23:28 -0400

    Create Round class and move methods into if from Game

--- rock_paper_scissors/game.rb
+++ rock_paper_scissors/game.rb
@@ -1,63 +1,15 @@
 require "./player"
 require "./judge"
+require "./round"
 
 class Game
-  CHOICES = Judge::WINNING_MOVE_AGAINST.keys
-  QUIT_MESSAGE = ["press q to quit"]
-
   def play
-    loop do start_game
-      if @users_choice == "Q"
-        break
-      else
-        display_win_count
-      end
-    end
+    round = Round.new
+    round.play
   end
 
-  def start_game
-    check_in_with_user
-    computer_chooses
-    run_outcome
-  end
-
-  def check_in_with_user
-    give_user_choices
-    get_user_choice
-  end
-
-  def give_user_choices
-    puts "Your move, what do you choose?"
-    options
-  end
-
-  def options
-    print "| #{CHOICES.join(" | ")} |"
-    puts
-    puts QUIT_MESSAGE
-  end
-
-  def get_user_choice
-    @users_choice = gets.chomp.upcase
-  end
-
-  def computer_chooses
-    @computers_play = CHOICES.sample
-  end
-
-  def run_outcome
-    display_outcome
-    judge_outcome
-  end
-
-  def display_outcome
-    puts "You played #{@users_choice}"
-    puts "The computer played #{@computers_play}"
-  end
-
-  def judge_outcome
-    judge = Judge.new(@users_choice, @computers_play)
-    judge.who_won?
+  def ask_to_play_again
+    
   end
 
   def display_win_count
