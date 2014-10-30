be89fd3d1f8fdb5caf102566128533d34708339e
Nick Clifford <ncliffor@gmail.com>
2014-10-30 13:51:57 -0400

    Create NullHotel class when no hotel is found

---
+++ hotels/null_hotel.rb
@@ -0,0 +1,21 @@
+class NullHotel
+  def name
+    "not found"
+  end
+
+  def phone_number
+    "not found"
+  end
+
+  def city
+    "not found"
+  end
+
+  def number_of_singles
+    "not found"
+  end
+
+  def number_of_doubles
+    "not found"
+  end
+end
