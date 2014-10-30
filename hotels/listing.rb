987eb41b9fcd89785878aafdeb302b8075b0d9e0
Nick Clifford <ncliffor@gmail.com>
2014-10-30 14:03:38 -0400

    Create Listing class to print hotel details

---
+++ hotels/listing.rb
@@ -0,0 +1,18 @@
+class Listing
+  def initialize(hotel)
+    @hotel = hotel
+  end
+
+  private
+  
+  attr_reader :hotel
+
+  def print_details
+    puts "Name: #{hotel.name}"
+    puts "City: #{hotel.city}"
+    puts "Phone Number: #{hotel.phone_number}"
+    puts "Single Rooms: #{hotel.single_beds}"
+    puts "Double Rooms: #{hotel.double_beds}"
+  end
+
+end
