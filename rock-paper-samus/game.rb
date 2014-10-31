712c86fa8d90df58a08c9691c8f7856deb090df3
Nick Clifford <ncliffor@gmail.com>
2014-10-31 13:43:21 -0400

    Add winning moves against hash

--- rock-paper-samus/game.rb
+++ rock-paper-samus/game.rb
@@ -1,3 +1,17 @@
 class Game
+  WINNING_MOVES_AGAINST = {
+    "Samus" => "Link",
+    "Link" => "Luigi",
+    "Luigi" => "Samus"
+  }
 
+
+
+  def play
+    puts "Welcome to rock, paper, samus"
+    
+  end
 end
+
+game = Game.new
+game.play
